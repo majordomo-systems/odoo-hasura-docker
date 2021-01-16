78b7f5809814e1e2c1aa99fe913d36648037d4d0
majordomo-systems <admin@majordomo.consulting>
2021-01-16 12:18:26 -0500

    corrected version

--- run.sh
+++ run.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 DESTINATION=$1
+
 # clone Odoo directory
 git clone -b 14.0 https://github.com/majordomo-systems/odoo-hasura-docker.git $DESTINATION
 rm -rf $DESTINATION/.git
 sudo chmod -R 777 $DESTINATION
+
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 docker exec --user root odoo-hasura-docker_odoo_1 pip3 install wheel
+
 # HOW TO INSTALL MODULES:
 # docker exec --user root odoo-hasura-docker_odoo_1 pip3 install #MODULE#
 # OR docker exec --user root odoo-hasura-docker_odoo_1 pip3 install -r /mnt/extra-addons/rest-framework/requirements.txt
-# set addon permissions and ownership
+
+# module permissions and ownership
 docker exec --user root odoo-hasura-docker_odoo_1 chown -R odoo:odoo /mnt/extra-addons
 docker exec --user root odoo-hasura-docker_odoo_1 chmod -R 777 /mnt/extra-addons
+
 # restart container
 # docker stop $(docker ps -aq)
 # docker-compose up -d
 
-echo "Odoo @ http://localhost:14069"
-echo "GraphQL Console @ http://localhost:80"
+ARRAY=$(hostname -I)
+echo ${ARRAY[*]}
+grep -o '^\S*' $ARRAY
+echo test
+IP=$(sed 's/\s.*$//' <<<"$ARRAY")
+echo $IP
+
+echo "Odoo @ http://localhost:14069 or http://$IP:14069"
+echo "GraphQL Console @ http://localhost:80 or http://$IP:80"
