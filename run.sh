e5c291cedc2c8d56cbc3b300cb815a3a87bfdbaf
majordomo-systems <admin@majordomo.consulting>
2021-01-16 12:34:11 -0500

    cleanup

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DESTINATION=$1
+ARRAY=$(hostname -I)
+IP=$(sed 's/\s.*$//' <<<"$ARRAY")
 
 # clone Odoo directory
 git clone -b 14.0 https://github.com/majordomo-systems/odoo-hasura-docker.git $DESTINATION
@@ -8,22 +10,15 @@ sudo chmod -R 777 $DESTINATION
 
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
-docker exec --user root odoo-hasura-docker_odoo_1 pip3 install wheel
 
 # HOW TO INSTALL MODULES:
 # docker exec --user root odoo-hasura-docker_odoo_1 pip3 install #MODULE#
 # OR docker exec --user root odoo-hasura-docker_odoo_1 pip3 install -r /mnt/extra-addons/rest-framework/requirements.txt
+docker exec --user root odoo-hasura-docker_odoo_1 pip3 install wheel
 
 # module permissions and ownership
 docker exec --user root odoo-hasura-docker_odoo_1 chown -R odoo:odoo /mnt/extra-addons
 docker exec --user root odoo-hasura-docker_odoo_1 chmod -R 777 /mnt/extra-addons
-
-# restart container
-# docker stop $(docker ps -aq)
-# docker-compose up -d
-
-ARRAY=$(hostname -I)
-IP=$(sed 's/\s.*$//' <<<"$ARRAY")
 
 clear
 echo "Odoo @ http://localhost:14069 or http://$IP:14069"
